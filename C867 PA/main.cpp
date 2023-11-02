93364a303157150d552fa014dd11197d67561fd3
Ryan Lee <ryanjlee98@gmail.com>
2023-11-02 15:18:08 -0700

    Initial Commit

---
+++ C867 PA/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C867 PA
+//
+//  Created by Ryan Lee on 11/2/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
